8fc517a3483fba617c5130fb5f889a9974c7be0b
Henric Andersson <ha@sensenet.nu>
2016-03-09 21:48:01 -0800

    Updated the directory and caching
    - Now properly shows files vs directories
    - Caching works and avoids hitting backend if folder is seen
    - refresh command added to force reload, drops all cached data

--- README.md
+++ README.md
@@ -15,50 +15,15 @@ A commandline based tool which enables you to:
 
 You must have `requests` installed (`pip install requests`)
 
+For improved console (CTRL-R, etc) you can install readline:
+* OSX: `pip install gnureadline`
+* Win: `pip install pyreadline`
+* Linux: Should work out-of-the-box
+
 # Usage
 
-`usage: stshell.py [-h] [--username EMAIL] [--password PASSWORD]
-                  [--server SERVER]
-                  type action [options [options ...]]`
-
-`--username` is your login to smartthings<br>
-`--password` is the password<br>
-These two can be stored in ~/.stshell in a key/value pair format<br>
-`--server` allows you to override the default graph.api.smartthings.com with whatever you want<br>
-`type` should be either `smartapp` or `devicetype` and determines what the action acts upon
-
-`action` must be one of the following:
-
-* `list`
-Lists all smartapps or devicetypes with their associated UUID
-* `contents <UUID>`
-Shows the contents of a smartapp or devicetype, including the UUIDs for each entry
-* `download <UUID>`
-Downloads the entire smartapp or devicetype into your current folder
-* `download <UUID> <UUID>`
-Downloads a part of a smartapp or devicetype into your current folder, use `contents` to get the second UUID
-* `delete <UUID>`
-Deletes an entire smartapp or devicetype. Will ask for confirmation before proceeding
-* `delete <UUID> <ITEM>`
-Deletes an item from the smartapp or devicetype. Will ask for confirmation before proceeding
-* `create <groovy file>`
-Creates a new smartapp or devicetype using the provided groovy file
-* `upload <UUID> <TYPE> <PATH> <FILE>`
-Uploads the FILE into smartapp or devicetype identified by UUID and tells the server it's TYPE and should be placed in the sub-directory of PATH. PATH can be an empty string "" to indicate root of folder. TYPE should be one of the following:
-** IMAGE
-** VIEW
-** JAVASCRIPT
-** I18N
-** CSS
-** OTHER
-and will give an error if you try something else.
-
-The tool will automatically detect conflicts and refuse to upload a file which would overwrite an existing file.
-
-* `update <UUID> <UUID> <FILE>
-Updates an item in a smartapp or devicetype with the contents in FILE. Use `contents` to get the second UUID
-If the update fails, it will print out why.
+Please see `stshell -h` for the most accurate an up-to-date explaination of options.
 
 # Notes
 
-Work in progress, still not able to update the groovy code, but that's next on the list.
+The `console` option is VERY new and is prone to break, right now it can traverse the entire server tree in a FTP like fashion. It implements `ls` and `cd` and soon `get`. It comes with a built-in help, just type `help`
--- classes/console.py
+++ classes/console.py
@@ -10,10 +10,10 @@ class ConsoleAccess(cmd.Cmd):
         """ Assigns a STServer Connection to the console """
         self.conn = conn
         self.cwd = ""
-        self.tree = {}
         # Prepopulate
-        self.tree["/smartapps"] = {"name" : "/smartapps", "uuid" : None, "parent" : None, "type" : None, "stale" : True}
-        self.tree["/devices"] = {"name" : "/devices", "uuid" : None, "parent" : None, "type" : None, "stale" : True}
+        self.tree = {}
+        self.tree["/smartapps"] = {"name" : "/smartapps", "dir" : True, "uuid" : None, "parent" : None, "type" : None, "stale" : True}
+        self.tree["/devices"] = {"name" : "/devices", "dir" : True, "uuid" : None, "parent" : None, "type" : None, "stale" : True}
         self.updatePrompt()
 
     def listBundle(self, node):
@@ -21,14 +21,12 @@ class ConsoleAccess(cmd.Cmd):
 
     def do_refresh(self, line):
         """ Marks all directories as stale, forcing a reload from server """
-        # format:
-        # name = "complete/path/to/file.groovy" (also used as key)
-        # uuid = "reference to actual file"
-        # type = "sa, dth"
-        # stale = True/False
-        #
-        # If stale is True, then this node should be reexplored
-        pass
+        # Prepopulate
+        print "Please wait, reloading..."
+        self.tree = {}
+        self.tree["/smartapps"] = {"name" : "/smartapps", "dir" : True, "uuid" : None, "parent" : None, "type" : None, "stale" : True}
+        self.tree["/devices"] = {"name" : "/devices", "dir" : True, "uuid" : None, "parent" : None, "type" : None, "stale" : True}
+        self.do_cd(self.cwd)
 
     def splitPath(self, path):
         path = path.split("/")
@@ -37,6 +35,15 @@ class ConsoleAccess(cmd.Cmd):
             if p:
                 new.append(p)
         return new
+
+    def generateTrail(self, filename, kind=None, parent=None, uuid=None):
+        """ Fills in the gaps in the directory structure """
+        parts = self.splitPath(filename)
+        cd = ""
+        for p in parts:
+            cd += "/" + p
+            if cd not in self.tree:
+                self.tree[cd] = {"name" : cd, "uuid" : uuid, "parent" : parent, "type" : kind, "stale" : False, "dir" : True}
 
     def loadList(self, base, force=False):
         if not self.tree[base]["stale"]:
@@ -50,19 +57,27 @@ class ConsoleAccess(cmd.Cmd):
         self.tree[base]["stale"] = False
         for d in data.values():
             filename = base + "/" + d["namespace"] + "/" + d["name"]
-            self.tree[filename] = {"name" : filename, "parent" : None, "uuid" : d["id"], "type" : None, "stale" : True}
+            self.tree[filename] = {"name" : filename, "dir" : True, "parent" : d["id"], "uuid" : None, "type" : None, "stale" : True}
+            self.generateTrail(filename)
 
     def loadItems(self, base, force=False):
         entry = self.tree[base]
-        if base.startswith("/smartapps/"):
-            kind = "sa"
-            data = self.conn.getSmartAppDetails(entry["uuid"])
-        elif base.startswith("/devices/"):
-            kind = "dth"
-            data = self.conn.getDeviceTypeDetails(entry["uuid"])
-        for k,v in data["flat"].iteritems():
-            filename = base + v
-            self.tree[filename] = {"name" : filename, "parent" : entry["uuid"], "uuid" : k, "type" : kind, "stale" : True}
+        if entry["stale"]:
+            if base.startswith("/smartapps/"):
+                kind = "sa"
+                data = self.conn.getSmartAppDetails(entry["parent"])
+            elif base.startswith("/devices/"):
+                kind = "dth"
+                data = self.conn.getDeviceTypeDetails(entry["parent"])
+            for k,v in data["flat"].iteritems():
+                filename = base + v
+                self.tree[filename] = {"name" : filename, "dir" : False, "parent" : entry["parent"], "uuid" : k, "type" : kind, "stale" : False}
+                self.generateTrail(filename, kind, entry["parent"])
+            # Also add static folders
+            for k in self.conn.UPLOAD_TYPE.values():
+                filename = base + "/" + k
+                self.generateTrail(filename, kind, entry["parent"])
+            entry["stale"] = False # Avoid loading this again
 
         #print repr(data)
 
@@ -108,14 +123,33 @@ class ConsoleAccess(cmd.Cmd):
         else:
             return cwd
 
-    def do_cd(self, line):
-        """ Changes the current folder """
-        cwd = self.resolvePath(line)
-        if cwd is None:
-            print "Path not found: " + line
-        else:
-            self.cwd = cwd
-            self.updatePrompt()
+    def partialPath(self, line):
+        error = False
+        parts = self.splitPath(line)
+        cwd = self.cwd
+        progress = False
+        results = []
+        if line[0] == "/":
+            cwd = ""
+        for part in parts:
+            paths = self.splitPath(cwd)
+            if part == ".." and len(paths):
+                cwd = ""
+                for i in range(0, len(paths)-1):
+                    cwd += "/" + paths[i]
+            elif part == "..":
+                break
+            else:
+                found = False
+                for t in self.tree:
+                    search = cwd + "/" + part
+                    if t.startswith(search):
+                        results.append(t)
+                        found = True
+                if found and cwd in self.tree and self.tree[cwd]["stale"]:
+                    self.loadFromServer(cwd)
+
+        return results
 
     def printFolderInfo(self, info):
         shown = {}
@@ -131,6 +165,56 @@ class ConsoleAccess(cmd.Cmd):
         for f in shown.values():
             print f
 
+    def emptyline(self):
+        pass
+
+    def do_pwd(self, line):
+        """ Shows current folder """
+        print("Current folder: "%s/" on %s" % (self.cwd, self.conn.URL_BASE))
+
+    def do_cd(self, line):
+        """ Changes the current folder """
+        if line == "":
+            self.do_pwd(line)
+            return
+
+        cwd = self.resolvePath(line)
+        if cwd is None:
+            print 'Path not found: "%s"' % line
+        else:
+            self.cwd = cwd
+            self.updatePrompt()
+
+    def disabled_complete_cd(self, text, line, begidx, endidx):
+        # Text contains whatever partial info the user has tapped
+        print 'DEBUG: Text = "%s", Line = "%s", begidx = "%s", endidx = "%s"' % (text, line, begidx, endidx)
+
+        # Scan tree for a matching item
+        if text != "":
+            cwd = self.resolvePath(text)
+        else:
+            cwd = self.cwd
+
+        if cwd is None:
+            cwd = self.cwd + "/" + text
+
+        print "CWD = " + cwd
+
+        self.loadFromServer(cwd)
+
+        # Iterate through tree, print all that matches
+        paths = self.splitPath(cwd)
+        result = []
+        #print "Looking for " + cwd
+        for t in self.tree:
+            if t.startswith(cwd) and self.tree[t]["uuid"] == None:
+                parts = self.splitPath(t)
+                result.append(parts[len(parts)-1] + "/")
+        return result
+
+    def complete_ls(self, text, line, begidx, endidx):
+        pass
+
     def do_ls(self, line):
         """ Shows the contents of current folder or the one provided as argument """
         folderinfo = []
@@ -139,6 +223,10 @@ class ConsoleAccess(cmd.Cmd):
             cwd = self.resolvePath(line)
         else:
             cwd = self.cwd
+
+        if cwd is None:
+            print 'Path not found: "%s"' % line
+            return
 
         # See if we need to load something from the server
         self.loadFromServer(cwd)
@@ -149,13 +237,40 @@ class ConsoleAccess(cmd.Cmd):
         for t in self.tree:
             if t.startswith(cwd + "/"):
                 parts = self.splitPath(t)
-                folderinfo.append(
-                    {"name" : parts[len(paths)],
-                    "dir" : (len(parts)-1) > len(paths)
-                    }
-                )
+                filename = parts[len(paths)]
+                info = {"name" : filename,
+                        "dir" : self.tree[cwd + "/" + filename]["dir"]
+                        }
+                folderinfo.append(info)
 
         self.printFolderInfo(folderinfo)
+
+    def do_dir(self, line):
+        """ Alias for ls """
+        return self.do_ls(line)
+
+    def do_debug(self, line):
+        print "DEBUG INFO - TREE:"
+        for v in self.tree.values():
+            if line == "" or line in repr(v):
+                print repr(v)
+
+    def do_get(self, line):
+        """ Downloads a file, does not yet support paths """
+        filename = self.cwd + "/" + line
+        if filename not in self.tree:
+            print 'ERROR: No such file "%s"' % filename
+            return
+        item = self.tree[filename]
+        if item["parent"] is None:
+            print 'ERROR: Cannot download "%s", it\'s a directory'
+            return
+
+        dstfile = os.path.basename(filename)
+        sys.stdout.write('Downloading "%s" ... ' % dstfile)
+        sys.stdout.flush()
+        if item["type"] == 'sa':
+            data = self.conn.downloadSmartAppItem(item["parent"], )
 
 
     def do_EOF(self, line):
